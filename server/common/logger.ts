dbe125a02eb6f173f90d694de2fb6ac9da5e1e92
jakobthomasson <jakob.thomasson@jayway.com>
2018-08-03 00:48:17 +0200

    add process.env.loglevel undefined check

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -10,9 +10,10 @@ console.log('');
 console.log('');
 console.log('');
 console.log(process.env.LOG_LEVEL)
+
 const l = pino({
   name: process.env.APP_ID,
-  level: process.env.LOG_LEVEL,
+  level: process.env.LOG_LEVEL || '',
 });
 
 export default l;
