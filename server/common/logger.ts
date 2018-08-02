58942572afe473c80d8c8c9f3a4af846fdba70f6
jakobthomasson <jakob.thomasson@jayway.com>
2018-08-03 00:52:10 +0200

    restore logger file and try to config dotenv with silent=true

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -1,19 +1,8 @@
 import * as pino from 'pino';
 
-console.log('');
-console.log('');
-
-console.log('');
-console.log('');
-console.log('');
-console.log('');
-console.log('');
-console.log('');
-console.log(process.env.LOG_LEVEL)
-
 const l = pino({
   name: process.env.APP_ID,
-  level: process.env.LOG_LEVEL || '',
+  level: process.env.LOG_LEVEL,
 });
 
 export default l;
