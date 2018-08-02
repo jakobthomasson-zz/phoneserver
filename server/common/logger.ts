3084a1b2ed0d7aa5d8dba08b4b46feaf2db895ac
jakobthomasson <jakob.thomasson@jayway.com>
2018-08-03 00:43:21 +0200

    no message

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -1,5 +1,15 @@
 import * as pino from 'pino';
 
+console.log('');
+console.log('');
+
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log(process.env.LOG_LEVEL)
 const l = pino({
   name: process.env.APP_ID,
   level: process.env.LOG_LEVEL,
